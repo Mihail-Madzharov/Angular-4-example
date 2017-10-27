292be6585b13b3e7733a75e32bd0afbeaf0d65ef
Mihail Madzharov <mihail.madzharov.m@gmail.com>
2017-10-27 21:15:38 +0300

    Added comments to the parent component

--- src/app/components/view-content-child/parent/parent.component.ts
+++ src/app/components/view-content-child/parent/parent.component.ts
@@ -19,21 +19,19 @@ export class ParentComponent implements AfterContentInit, AfterViewInit {
     }
 
     public ngAfterContentInit(): void {
-        debugger
-        if (this.divChildren != null) {
-
-            this.divChildren.forEach(a => console.log(a.text))
-        }
+        // it doesn't exist in here 
+        // this.divChildren.forEach(a => console.log(a.text))
         // this div doesn't exist yet
-        // this.viewChild.viewChildColor = true;
+        this.viewChild.viewChildColor = true;
     }
 
     public ngAfterViewInit(): void {
-        debugger
+        // it does exist in here
+        // this.divChildren.forEach(a => console.log(a.text))
         
         // you can't change the value in after view init
         // you can if you call change detection
-            // this.divChildren.forEach((a, index) => a.text = index.toString())
-            // this.viewChild.viewChildColor = false;
+        // this.divChildren.forEach((a, index) => a.text = index.toString())
+        // this.viewChild.viewChildColor = false;
     }
 }
